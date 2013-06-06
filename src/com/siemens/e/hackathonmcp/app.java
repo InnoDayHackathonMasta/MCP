de5561bb186fa65d96be1988f7f2e1eee78474c3
Denis Griesmeyer <denis.griesmeyer@freenet.de>
2013-06-06 12:33:27 +0200

    Initial Check In

---
+++ src/com/siemens/e/hackathonmcp/app.java
@@ -0,0 +1,10 @@
+package com.siemens.e.hackathonmcp;
+
+import android.app.Application;
+
+public class app extends Application {
+	
+	public static final String BT_UUID_CHAIN = "201BB686-672C-46FA-B86B-B9BA8BAACE71";
+	public static final String BT_UUID_CLIENTINFO = "7F814AFE-90D0-4768-ADAD-89FD2F7F851A";
+	
+}
