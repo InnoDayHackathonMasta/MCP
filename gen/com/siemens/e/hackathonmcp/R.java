3c23b93b21828c95885830a9aa264c8d36829c22
Denis Griesmeyer <denis.griesmeyer@freenet.de>
2013-06-12 23:08:30 +0200

    added logOutput
    removed one unneded thread and added log output handling so one can see
    connecting devices

--- gen/com/siemens/e/hackathonmcp/R.java
+++ gen/com/siemens/e/hackathonmcp/R.java
@@ -24,10 +24,36 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f080000;
-        public static final int action_settings=0x7f080003;
-        public static final int main_btn_start_mcp=0x7f080002;
-        public static final int textView1=0x7f080001;
+        public static final int TableLayout1=0x7f080000;
+        public static final int action_settings=0x7f08001d;
+        public static final int main_btn_be_mcp=0x7f08000a;
+        public static final int main_btn_kill_chain=0x7f08000b;
+        public static final int main_btn_manualtext_send=0x7f080015;
+        public static final int main_btn_reorder_send=0x7f080010;
+        public static final int main_btn_start=0x7f080017;
+        public static final int main_btn_status_0_0=0x7f080005;
+        public static final int main_btn_status_0_1=0x7f080007;
+        public static final int main_btn_status_0_2=0x7f080009;
+        public static final int main_btn_stop=0x7f080018;
+        public static final int main_edt_manualtext=0x7f080014;
+        public static final int main_edt_reordertext=0x7f08000f;
+        public static final int main_txt_chain_overview_header=0x7f080002;
+        public static final int main_txt_log_output=0x7f08001c;
+        public static final int main_txt_log_output_header=0x7f08001a;
+        public static final int main_txt_manualtext_header=0x7f080012;
+        public static final int main_txt_reorder_header=0x7f08000d;
+        public static final int tableRow1=0x7f080001;
+        public static final int tableRow10=0x7f080004;
+        public static final int tableRow11=0x7f080006;
+        public static final int tableRow12=0x7f080008;
+        public static final int tableRow2=0x7f080003;
+        public static final int tableRow3=0x7f08000c;
+        public static final int tableRow4=0x7f08000e;
+        public static final int tableRow5=0x7f080011;
+        public static final int tableRow6=0x7f080013;
+        public static final int tableRow7=0x7f080016;
+        public static final int tableRow8=0x7f080019;
+        public static final int tableRow9=0x7f08001b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
